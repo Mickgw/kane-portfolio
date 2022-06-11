56223d123b0889c4f716c38b416513268495a6a7
Mickgw <mick.waanders@upcmail.nl>
2022-06-11 21:26:26 +0200

    f

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,11 +2,11 @@
 // import test from "../content/photo-albums.json"
 
 
-function importAll(r) {
-  return r.keys().map(r);
-}
+// function importAll(r) {
+//   return r.keys().map(r);
+// }
 
-const images = importAll(require.context('../content/photo-albums', false, /\.(png|jpe?g|svg)$/));
+// const images = importAll(require.context('../content/photo-albums', false, /\.(png|jpe?g|svg)$/));
 
 const Work = () => {
 
