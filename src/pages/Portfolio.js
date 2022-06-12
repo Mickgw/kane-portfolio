b64a5e834f8f1dce6a6379617d2cd7588b76d96b
Mickgw <mick.waanders@upcmail.nl>
2022-06-12 16:34:47 +0200

    f

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,6 @@
 
-// import test from "../content/photo-albums.json"
+import test from "../content/photo-albums.json"
 
-
-// function importAll(r) {
-//   return r.keys().map(r);
-// }
-
-// const images = importAll(require.context('../content/photo-albums', false, /\.(png|jpe?g|svg)$/));
 
 const Work = () => {
 
