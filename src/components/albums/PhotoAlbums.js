7546af593141e28ab25a9c8b19476eeca34da35b
Mickgw <mick.waanders@upcmail.nl>
2022-06-14 12:17:36 +0200

    f

--- src/components/albums/PhotoAlbums.js
+++ src/components/albums/PhotoAlbums.js
@@ -2,7 +2,7 @@
 import photoAlbums from "../../content/photo-albums.json";
 import { Link } from "react-router-dom";
 
-import test from "../../assets/images/uploads/portrait-placeholder2.jpeg"
+import test from "../../assets/images/uploads/portrait-placeholder2.jpeg";
 
 const PhotoAlbums = () => {
   return (
@@ -13,28 +13,29 @@ const PhotoAlbums = () => {
       <div className="photo-albums">
         <div className="container">
           <div className="album-card-grid">
-            {photoAlbums.album_list.length && photoAlbums.album_list.map((album, i) => {
-              console.log("../../assets" + album.image)
-              console.log(test)
-              return (
-                <>
-                  <div key={i} className="album-card">
-                    <div
-                      className="album-card-bg"
-                      style={{ backgroundImage: `url(${album.image})` }}
-                    ></div>
-                    <div className="album-card-cover-content">
-                      <span className="content-year-date">{album.year}</span>
-                      <h2 className="content-header">
-                        <Link className="links" to={`/albums/${album.id}`}>
-                          {album.title}
-                        </Link>
-                      </h2>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
+            {photoAlbums.album_list.length &&
+              photoAlbums.album_list.map((album, i) => {
+                console.log("../../assets" + album.image);
+                console.log(test);
+                return (
+                  <>
+                    <Link className="links" to={`/albums/${album.title.replace(" ", "-").toLowerCase()}`}>
+                      <div key={i} className="album-card">
+                        <div
+                          className="album-card-bg"
+                          style={{ backgroundImage: `url(${album.image})` }}
+                        ></div>
+                        <div className="album-card-cover-content">
+                          <span className="content-year-date">
+                            {album.year}
+                          </span>
+                          <h2 className="content-header">{album.title}</h2>
+                        </div>
+                      </div>
+                    </Link>
+                  </>
+                );
+              })}
           </div>
         </div>
       </div>
