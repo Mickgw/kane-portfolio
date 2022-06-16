47196668f09ed2b9bfbc10d920514a4cfc3e3e79
Mickgw <mick.waanders@upcmail.nl>
2022-06-16 13:42:34 +0200

    f

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,39 +1,39 @@
 import { useParams, NavLink } from "react-router-dom";
 import { useEffect } from "react";
-import { useInView } from "react-intersection-observer";
-import { motion, useAnimation } from "framer-motion";
+// import { useInView } from "react-intersection-observer";
+// import { motion, useAnimation } from "framer-motion";
 
-import portfolioImages from "../assets/images/images-for-portfolio/portfolio-images.json";
+// import portfolioImages from "../assets/images/images-for-portfolio/portfolio-images.json";
 
-function FadeInWhenVisible({ children }) {
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
+// function FadeInWhenVisible({ children }) {
+//   const controls = useAnimation();
+//   const [ref, inView] = useInView();
 
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
-    }
-  }, [controls, inView]);
+//   useEffect(() => {
+//     if (inView) {
+//       controls.start("visible");
+//     }
+//   }, [controls, inView]);
 
-  return (
-    <motion.div
-      ref={ref}
-      animate={controls}
-      initial="hidden"
-      transition={{ duration: 1, delay: 0.5 }}
-      variants={{
-        hidden: {
-          opacity: 0,
-          y: "5vh",
-          transition: { ease: [0.2, 0.2, -0.05, 0.95] },
-        },
-        visible: { opacity: 1, y: 0 },
-      }}
-    >
-      {children}
-    </motion.div>
-  );
-}
+//   return (
+//     <motion.div
+//       ref={ref}
+//       animate={controls}
+//       initial="hidden"
+//       transition={{ duration: 1, delay: 0.5 }}
+//       variants={{
+//         hidden: {
+//           opacity: 0,
+//           y: "5vh",
+//           transition: { ease: [0.2, 0.2, -0.05, 0.95] },
+//         },
+//         visible: { opacity: 1, y: 0 },
+//       }}
+//     >
+//       {children}
+//     </motion.div>
+//   );
+// }
 
 const Portfolio = () => {
   const { tag } = useParams();
