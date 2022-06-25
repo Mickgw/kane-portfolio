86edd2a6e6f09f00a43f7cbb07d9ab06a75bbf08
Mickgw <mick.waanders@upcmail.nl>
2022-06-25 15:46:20 +0200

    banner on home page animation and css styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,101 +3,109 @@ import Image2 from "../assets/images/portrait-placeholder1.jpeg";
 import Image3 from "../assets/images/image-3-banner.jpeg";
 
 import { useEffect, useState } from "react";
-// import { gsap } from "gsap";
+import { gsap, Power3 } from "gsap";
 
-
-const Banner2 = () => {
-  const [showShowScrollDown, setShowScrollDown] = useState(false);
-  // const ImageDuration = 1;
-
-  // const scrollDownTextAnimation = {
-  //   exit: {
-  //     y: 100,
-  //     opacity: 0,
-  //   },
-  // };
+const Banner = () => {
+  const imageContainerAnimationDuration = 1.5;
+  const staggerDuration = 0.5;
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.pageYOffset < 200) {
-        setShowScrollDown(true);
-      } else {
-        setShowScrollDown(false);
-      }
-    });
-  }, []);
+    gsap
+      .timeline()
+      .to("#banner", { autoAlpha: 1, duration: 0 }) //Prevents white flash
+      .fromTo(
+        "#banner-image-container",
+        { y: 200, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: imageContainerAnimationDuration,
+          stagger: staggerDuration,
+          ease: Power3.easeOut,
+        }
+      )
 
-  // useEffect(() => {
-  //   gsap
-  //     .timeline()
-  //     .to("#banner", { autoAlpha: 1, duration: 0 }) //Prevents white flash
-
-  //     .fromTo(
-  //       "#reveal-image-left",
-  //       {
-  //         y: 100,
-  //         opacity: 0,
-  //       },
-  //       { delay: 1, y: 0, opacity: 1, duration: ImageDuration }
-  //     )
-  //     .fromTo(
-  //       "#reveal-image-right",
-  //       {
-  //         y: 100,
-  //         opacity: 0,
-  //       },
-  //       { y: 0, opacity: 1, delay: -0.5, duration: ImageDuration }
-  //     )
-  //     .fromTo(
-  //       "#reveal-middle-image",
-  //       { y: 100, opacity: 0 },
-  //       { y: 0, opacity: 1, delay: -0.5, duration: ImageDuration }
-  //     )
-  //     .fromTo(
-  //       "#reveal-banner-text",
-  //       { opacity: 0 },
-  //       { duration: 3, opacity: 1 }
-  //     )
-  // });
+      .fromTo("#banner-image-container-middle", {y: 200, opacity: 0}, {y: 0, opacity: 1, duration: imageContainerAnimationDuration, delay: -(imageContainerAnimationDuration - staggerDuration), ease: Power3.easeOut})
+      .fromTo(
+        "#banner-image-left",
+        { scale: 2, skewY: 5 },
+        {
+          scale: 1,
+          skewY: 0,
+          duration: 1.5,
+          delay: -2.5,
+        }
+      )
+      .fromTo(
+        "#banner-image-right",
+        { scale: 2, skewY: -5 },
+        {
+          scale: 1,
+          skewY: 0,
+          duration: 1.5,
+          delay: -2.5 + staggerDuration,
+        }
+      )
+      .fromTo(
+        "#banner-image-middle",
+        { y: 200, scale: 1.3},
+        {
+          y: 0,
+          scale: 1,
+          duration: 1.5,
+          delay: -2.5 + (staggerDuration * 2)
+        }
+      )
+      .fromTo("#reveal-banner-text", {opacity: 0, skewY: 10}, {opacity: 1, skewY: 0, duration: 1, delay: -.5});
+  });
 
   return (
     <div id="banner" className="banner">
       <div className="container">
         <div className="row">
           <div id="reveal-banner-text" className="banner-text">
-            {/* <Marquee className="banner-marquee" gradient={false} pauseOnHover={true}>
-              kane jansen
-            </Marquee> */}
-            kane jansen
+            kane jansen        
           </div>
           <div className="section left">
             <div className="inner-section">
-              <img
-                id="reveal-image-left"
-                className="side-image left"
-                src={Image1}
-                alt="left"
-              />
+              <div
+                id="banner-image-container"
+                className="banner-images-container"
+              >
+                <img
+                  id="banner-image-left"
+                  className="side-image left"
+                  src={Image1}
+                  alt="left"
+                />
+              </div>
             </div>
             <div className="inner-section" />
           </div>
           <div id="middle-image-container" className="section middle">
-            <img
-              id="reveal-middle-image"
-              className="banner-image"
-              src={Image2}
-              alt="middle"
-            />
+            <div id="banner-image-container-middle" className="banner-images-container">
+              <img
+                id="banner-image-middle"
+                className="banner-image"
+                src={Image2}
+                alt="middle"
+              />
+            </div>
           </div>
           <div className="section right">
             <div className="inner-section" />
             <div className="inner-section">
-              <img
-                id="reveal-image-right"
-                className="side-image right"
-                src={Image3}
-                alt="right"
-              />
+              <div
+                id="banner-image-container"
+                className="banner-images-container"
+              >
+                <img
+                  id="banner-image-right"
+                  className="side-image right"
+                  src={Image3}
+                  alt="right"
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -120,4 +128,4 @@ const Banner2 = () => {
   );
 };
 
-export default Banner2;
+export default Banner;
