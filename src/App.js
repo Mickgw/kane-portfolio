c5fdef9a8cc3357ffcb12587356cb062da21d944
Mickgw <mick.waanders@upcmail.nl>
2022-06-26 23:18:28 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./scss/main.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { useState } from "react";
 
 // Components
 import Navbar from "./components/Navbar";
@@ -14,34 +13,13 @@ import Motion from "./pages/Motion";
 import PhotoAlbumsPage from "./pages/PhotoAlbumsPage";
 import PageNotFound from "./pages/PageNotFound";
 
-// Custom cursor
-import CustomCursor from "custom-cursor-react";
-import "custom-cursor-react/dist/index.css";
 
 function App() {
-  // const [cursorX, setCursorX ] = useState();
-  // const [cursorY, setCursorY ] = useState();
 
-  // window.addEventListener('mousedown', (e) => {
-  //   setCursorX(e.pageX)
-  //   setCursorY(e.pageY)
-  // })
 
   return (
     <>
       <div className="App">
-        <CustomCursor
-          targets={["button", ".logo", "a"]}
-          customClass="custom-cursor"
-          dimensions={40}
-          fill="white"
-          opacity={1}
-          smoothness={{
-            movement: 0.8,
-            scale: 0.1,
-          }}
-          targetScale={4}
-        />
         <Router>
           <header>
             <Navbar />
@@ -54,21 +32,12 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/motion" element={<Motion />} />
               <Route path="/albums/:title" element={<PhotoAlbumsPage />} />
-              <Route path="*" element={<PageNotFound />} />
+              <Route path="/404" element={<PageNotFound />} />
             </Routes>
           </main>
 
-          <footer>
-            <Footer />
-          </footer>
 
-          {/* <div
-            className="custom-cursor"
-            style={{
-              left: cursorX + "px",
-              top: cursorY + "px",
-            }}
-          ></div> */}
+            <Footer />
         </Router>
       </div>
     </>
