e76a630f70e51ae4143270f10c1251dd3ac1b686
Mickgw <mick.waanders@upcmail.nl>
2022-07-05 16:09:47 +0200

    added content to motion page, can now add videos

--- src/pages/Motion.js
+++ src/pages/Motion.js
@@ -1,4 +1,5 @@
 import videoProjects from "../assets/videos/motion-videos.json";
+import FadeInWhenVisible from "../components/hooks/FadeInWhenVisible";
 
 const Motion = () => {
   return (
@@ -8,13 +9,15 @@ const Motion = () => {
           <div className="video-grid">
             {videoProjects.video_projects_list.map((video, index) => {
               return (
-                <div className="video-container" key={index}>
-                  <h2 className="video-title">{video.title}</h2>
-                  <p className="video-description">{video.description}</p>
-                  <video width="100%" height="100%" controls>
-                    <source src={video.video} />
-                  </video>
-                </div>
+                <FadeInWhenVisible>
+                  <div className="video-container" key={index}>
+                    <h2 className="video-title">{video.title}</h2>
+                    <p className="video-description">{video.description}</p>
+                    <video controls>
+                      <source src={video.video_url} type="video/mp4" />
+                    </video>
+                  </div>
+                </FadeInWhenVisible>
               );
             })}
           </div>
