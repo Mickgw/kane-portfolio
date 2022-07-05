deb77c0b18144b5724dba6bc59782f40976336b9
Mickgw <mick.waanders@upcmail.nl>
2022-07-05 14:58:19 +0200

    f

--- src/pages/Motion.js
+++ src/pages/Motion.js
@@ -1,8 +1,25 @@
-import React from "react";
+import videoProjects from "../assets/videos/motion-videos.json";
 
 const Motion = () => {
   return (
     <>
+      <div className="motion">
+        <div className="container">
+          <div className="video-grid">
+            {videoProjects.video_projects_list.map((video, index) => {
+              return (
+                <div className="video-container" key={index}>
+                  <h2 className="video-title">{video.title}</h2>
+                  <p className="video-description">{video.description}</p>
+                  <video width="100%" height="100%" controls>
+                    <source src={video.video} />
+                  </video>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
