8c36ea905ffb0763d4ba9d47e541135b6ae183a3
Mickgw <mick.waanders@upcmail.nl>
2022-08-12 14:44:46 +0200

    changed up banner animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,79 +4,110 @@ import Image3 from "../assets/images/image-3-banner.jpeg";
 
 import { useEffect } from "react";
 import { gsap, Power3 } from "gsap";
+import { motion } from "framer-motion";
 
 import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Banner = () => {
-  const imageContainerAnimationDuration = 1.5;
-  const staggerDuration = 0.5;
+  const imagesAnimation = {
+    hidden: {
+      y: "15vh",
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 1,
+        transition: { ease: [0.2, 0.2, -0.05, 0.95] },
+      }
+    },
+  }
 
-  useEffect(() => {
-    gsap
-      .timeline()
-      .to("#banner", { autoAlpha: 1, duration: 0 }) //Prevents white flash
-      .fromTo(
-        "#banner-image-container",
-        { y: 200, opacity: 0 },
-        {
-          y: 0,
-          opacity: 1,
-          duration: imageContainerAnimationDuration,
-          stagger: staggerDuration,
-          ease: Power3.easeOut,
-          delay: 0.5,
-        }
-      )
+  const textAnimation = {
+    hidden: {
+      y: 50,
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: .1,
+        duration: 1,
+        transition: { ease: [0.2, 0.2, -0.05, 0.95] },
+      }
+    },
+  }
+  // const imageContainerAnimationDuration = 1.5;
+  // const staggerDuration = 0.5;
 
-      .fromTo(
-        "#banner-image-container-middle",
-        { y: 200, opacity: 0 },
-        {
-          y: 0,
-          opacity: 1,
-          duration: imageContainerAnimationDuration,
-          delay: -(imageContainerAnimationDuration - staggerDuration),
-          ease: Power3.easeOut,
-        }
-      )
-      .fromTo(
-        "#banner-image-left",
-        { scale: 2, skewY: 5 },
-        {
-          scale: 1,
-          skewY: 0,
-          duration: 1.5,
-          delay: -2.5,
-        }
-      )
-      .fromTo(
-        "#banner-image-right",
-        { scale: 2, skewY: -5 },
-        {
-          scale: 1,
-          skewY: 0,
-          duration: 1.5,
-          delay: -2.5 + staggerDuration,
-        }
-      )
-      .fromTo(
-        "#banner-image-middle",
-        { y: 200, scale: 1.3 },
-        {
-          y: 0,
-          scale: 1,
-          duration: 1.5,
-          delay: -2.5 + staggerDuration * 2,
-        }
-      )
-      .fromTo(
-        "#reveal-banner-text",
-        { opacity: 0, skewY: 10, scale: 0.96 },
-        { opacity: 1, skewY: 0, scale: 1, duration: 1, delay: -0.5 }
-      );
-  });
+  // useEffect(() => {
+  //   gsap
+  //     .timeline()
+  //     .to("#banner", { autoAlpha: 1, duration: 0 }) //Prevents white flash
+  //     .fromTo(
+  //       "#banner-image-container",
+  //       { y: 200, opacity: 0 },
+  //       {
+  //         y: 0,
+  //         opacity: 1,
+  //         duration: imageContainerAnimationDuration,
+  //         stagger: staggerDuration,
+  //         ease: Power3.easeOut,
+  //         delay: 0.5,
+  //       }
+  //     )
+
+  //     .fromTo(
+  //       "#banner-image-container-middle",
+  //       { y: 200, opacity: 0 },
+  //       {
+  //         y: 0,
+  //         opacity: 1,
+  //         duration: imageContainerAnimationDuration,
+  //         delay: -(imageContainerAnimationDuration - staggerDuration),
+  //         ease: Power3.easeOut,
+  //       }
+  //     )
+  //     .fromTo(
+  //       "#banner-image-left",
+  //       { scale: 2, skewY: 5 },
+  //       {
+  //         scale: 1,
+  //         skewY: 0,
+  //         duration: 1.5,
+  //         delay: -2.5,
+  //       }
+  //     )
+  //     .fromTo(
+  //       "#banner-image-right",
+  //       { scale: 2, skewY: -5 },
+  //       {
+  //         scale: 1,
+  //         skewY: 0,
+  //         duration: 1.5,
+  //         delay: -2.5 + staggerDuration,
+  //       }
+  //     )
+  //     .fromTo(
+  //       "#banner-image-middle",
+  //       { y: 200, scale: 1.3 },
+  //       {
+  //         y: 0,
+  //         scale: 1,
+  //         duration: 1.5,
+  //         delay: -2.5 + staggerDuration * 2,
+  //       }
+  //     )
+  //     .fromTo(
+  //       "#reveal-banner-text",
+  //       { opacity: 0, skewY: 10, scale: 0.96 },
+  //       { opacity: 1, skewY: 0, scale: 1, duration: 1, delay: -0.5 }
+  //     );
+  // });
 
   // useEffect(() => {
   //   gsap
@@ -95,15 +126,16 @@ const Banner = () => {
     <div id="banner" className="banner">
       <div className="container">
         <div className="row">
-          <div id="reveal-banner-text" className="banner-text">
+          <motion.div  variants={textAnimation}  initial="hidden" animate="visible" id="reveal-banner-text" className="banner-text">
             Kane Jansen
-          </div>
+          </motion.div>
 
           <div className="section left">
             <div className="inner-section">
-              <div
+              <motion.div
                 id="banner-image-container"
                 className="banner-images-container"
+                variants={imagesAnimation}  initial="hidden" animate="visible"
               >
                 <img
                   id="banner-image-left"
@@ -111,14 +143,15 @@ const Banner = () => {
                   src={Image1}
                   alt="left"
                 />
-              </div>
+              </motion.div>
             </div>
             <div className="inner-section" />
           </div>
           <div id="middle-image-container" className="section middle">
-            <div
+            <motion.div
               id="banner-image-container-middle"
               className="banner-images-container"
+              variants={imagesAnimation}  initial="hidden" animate="visible"
             >
               <img
                 id="banner-image-middle"
@@ -126,14 +159,15 @@ const Banner = () => {
                 src={Image2}
                 alt="middle"
               />
-            </div>
+            </motion.div>
           </div>
           <div className="section right">
             <div className="inner-section" />
             <div className="inner-section">
-              <div
+              <motion.div
                 id="banner-image-container"
                 className="banner-images-container"
+                variants={imagesAnimation}  initial="hidden" animate="visible"
               >
                 <img
                   id="banner-image-right"
@@ -141,7 +175,7 @@ const Banner = () => {
                   src={Image3}
                   alt="right"
                 />
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
