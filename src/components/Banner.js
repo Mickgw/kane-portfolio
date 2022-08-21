f8333de4380a0d88bcefa34b9f3322420d1de800
Mickgw <mick.waanders@upcmail.nl>
2022-08-22 00:50:11 +0200

    nothing special

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,12 @@ import bigBannerImage1 from "../assets/images/banner_V2_image1.jpg";
 import bigBannerImage2 from "../assets/images/banner_V2_image2.jpg";
 import bigBannerImage3 from "../assets/images/banner_V2_image3.jpg";
 
-import { gsap, Power3} from "gsap";
+import { gsap, Power3 } from "gsap";
 import { useEffect } from "react";
 import { motion } from "framer-motion";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 const BannerV2 = () => {
   var images = [bigBannerImage1, bigBannerImage2, bigBannerImage3];
@@ -17,16 +17,35 @@ const BannerV2 = () => {
     gsap
       .timeline()
       .to("#banner", { autoAlpha: 1, duration: 0 })
-      .fromTo("#banner-reveal", { height: "100%"}, { height: "0%", duration: 1.2, delay: .4, ease: Power3.easeOut })
+      .fromTo(
+        "#banner-reveal",
+        { height: "100%" },
+        { height: "0%", duration: 1.2, delay: 0.4, ease: Power3.easeOut }
+      )
       .fromTo(
         "#banner-image",
         { scale: 1.5 },
         { scale: 1, duration: 1.5 },
         "-=1.5"
       )
-      .fromTo("#my-name", {y: 100 }, {y: 0, duration: 1, ease: Power3.easeOut}, "-=.3")
-      .fromTo("#my-profession", {y: 100 }, {y: 0, duration: 1, ease: Power3.easeOut}, "-=.9")
-      .fromTo("#scrolldown", {y: 100 }, {y: 0, duration: 1, ease: Power3.easeOut}, "-=.9")
+      .fromTo(
+        "#my-name",
+        { y: 100 },
+        { y: 0, duration: 1, ease: Power3.easeOut },
+        "-=.3"
+      )
+      .fromTo(
+        "#my-profession",
+        { y: 100 },
+        { y: 0, duration: 1, ease: Power3.easeOut },
+        "-=.9"
+      )
+      .fromTo(
+        "#scrolldown",
+        { y: 100 },
+        { y: 0, duration: 1, ease: Power3.easeOut },
+        "-=.9"
+      );
   });
 
   return (
@@ -36,7 +55,9 @@ const BannerV2 = () => {
           <div className="hero-text">
             <div className="hero-text-inner">
               <div className="my-name-line">
-                <h1 className="my-name-text" id="my-name">Kane Jansen</h1>
+                <h1 className="my-name-text" id="my-name">
+                  Kane Jansen
+                </h1>
               </div>
               <div className="profession-line">
                 <h3 className="profession-text" id="my-profession">
@@ -44,20 +65,29 @@ const BannerV2 = () => {
                 </h3>
               </div>
               <div className="scrolldown-line">
-                <span className="scrolldown-text" id="scrolldown">scroll </span>
-                <FontAwesomeIcon icon={faAngleDown} className="arrow-down-icon" id="scrolldown"/>
+                <span className="scrolldown-text" id="scrolldown">
+                  scroll{" "}
+                </span>
+                <FontAwesomeIcon
+                  icon={faAngleDown}
+                  className="arrow-down-icon"
+                  id="scrolldown"
+                />
               </div>
             </div>
           </div>
-          <motion.div className="big-banner-image" id="banner-image-container">
+          <div className="big-banner-image" id="banner-image-container">
             <div className="banner-reveal" id="banner-reveal" />
-            <img src={banner_image} alt="big-banner-car" id="banner-image" />
-          </motion.div>
+            <motion.img
+              src={banner_image}
+              alt="big-banner-car"
+              id="banner-image"
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default BannerV2;
